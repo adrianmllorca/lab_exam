3358ca417901b44fe1cc843b2a1ab6c200fd640f
Adrian Miguel Llorca <apllorca2@up.edu.ph>
2025-04-21 15:59:06 +0800

    updated repository

--- Llorca_B9_answer/llorca_b9.py
+++ Llorca_B9_answer/llorca_b9.py
@@ -6,13 +6,15 @@ df.columns = new_col_names
 
 replicates = df['replicate']
 interval = df['interval']
+count = df['count']
 
-replicate_interval = pd.DataFrame({'replicate' : replicates,
-                                   'interval' : interval}).drop_duplicates().sort_values('replicate')
+filtered_df = pd.DataFrame({'replicate' : replicates,
+                                   'interval' : interval,
+                                   'count' : count}).drop_duplicates().sort_values('replicate')
 
-# output_df =df.groupby(['replicate', 'interval']).mean()
+average_count = df.groupby()
 
 # output_df = pd.DataFrame({'replicate_interval' : replicate_interval})
 
 # output_df.to_csv('b9_output1.csv')
-replicate_interval.to_csv('b9_output1.csv', index=False)
+filtered_df.to_csv('b9_output1.csv', index=False)
